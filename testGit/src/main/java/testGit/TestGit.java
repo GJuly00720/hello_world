f167b208ba1b3ffe3369d7f62a10b9201a7374d6
PC13 <PC13@PC13-PC>
2018-05-21 12:34:56 +0900

    무시파일
    ㄱㄱ

--- testGit/src/main/java/testGit/TestGit.java
+++ testGit/src/main/java/testGit/TestGit.java
@@ -2,7 +2,7 @@ package testGit;
 
 public class TestGit {
 	public static void main(String[] args) {
-		String test = "testGit, 자고싶네여;";
+		String test = "testGit, 222222";
 		System.out.println(test);
 	}
 }
